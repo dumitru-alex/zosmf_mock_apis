effaff8b29b5778b9fb9cbc6d368ac21659fb790
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-03-24 17:36:10 +0100

    restructure server.js add utils file

--- server.js
+++ server.js
@@ -1,46 +1,66 @@
+/**
+ * Module dependencies
+ */
 const jsonServer = require("json-server");
 const path = require("path");
-const routes = require(path.join(__dirname, "routes.json"));
 const https = require("https");
 const fs = require("fs");
+const utils = require("./utils");
 
-const DEFAULT_PORT = 3000;
-
+/**
+ * Create the server
+ */
 const server = jsonServer.create();
 
-const keyFile = path.join(__dirname, "key.pem");
-const certFile = path.join(__dirname, "cert.pem");
+/**
+ * Get port from environment
+ */
+const DEFAULT_PORT = 3000;
+const port = utils.normalizePort(process.env.PORT || DEFAULT_PORT);
 
+
+/**
+ * Import the external routing and seed file
+ */
+const routes = require(path.join(__dirname, "routes.json"));
 const db = path.join(__dirname, "db.json");
+
+
+/**
+ * Server setup
+ */
 const router = jsonServer.router(db);
 const rewriter = jsonServer.rewriter(routes);
 const middlewares = jsonServer.defaults();
 
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
-
-
-// Inline rewriter
-// server.use(jsonServer.rewriter({
-//     '/zosmf/*' : "/$1",
-//     '/restjobs/jobs/:jobName/:jobId/files' : '/jobs?jobname=:jobName&jobid=:jobId'
-// }));
-
 // Order matters! rewriter needs to be before router 
 server.use(rewriter);
 // mount the router on a different endpoint
 server.use(router);
 
-const port = process.env.PORT || DEFAULT_PORT;
-
-https
-.createServer(
+/**
+ * Wrap the json server with TLS layer
+ */
+const keyFile = path.join(__dirname, "key.pem");
+const certFile = path.join(__dirname, "cert.pem");
+const httpsServer = https.createServer(
     {
-    key: fs.readFileSync(keyFile),
-    cert: fs.readFileSync(certFile)
+        key: fs.readFileSync(keyFile),
+        cert: fs.readFileSync(certFile)
     },
     server
-).listen(port, () => {
-    console.log(`z/OSMF Mock Server is running on port ${port}`);
-})
+    );
+    
+httpsServer.listen(port, () => {
 
+    var addr = httpsServer.address();
+    // Change address name to localhost for IPv6  
+    addr.address = "::" ? "localhost" : addr.address;
+
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    console.log(`z/OSMF Mock Server details:\nAddress: ${addr.address}\nFamily: ${addr.family}\nListening on: ${bind}`); 
+});
---
+++ utils.js
@@ -0,0 +1,15 @@
+exports.normalizePort = (value) => {
+    var port = parseInt(value, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return value;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+};

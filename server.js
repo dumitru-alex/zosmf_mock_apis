f41fa5fdcf6917f3a79899bb51bfc41386050ce7
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-03-25 17:30:22 +0100

    WIP implementing job filter with wildcard
    - use case not covered by json-server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const https = require("https");
 const fs = require("fs");
 const utils = require("./utils");
+const _ = require("lodash");
 
 /**
  * Create the server
@@ -22,9 +23,9 @@ const port = utils.normalizePort(process.env.PORT || DEFAULT_PORT);
 /**
  * Import the external routing and seed file
  */
-const routes = require(path.join(__dirname, "routes.json"));
+const routes = utils.generateRoutes(path.join(__dirname, "routes.json"));
 const db = path.join(__dirname, "db.json");
-
+const dbInMem = JSON.parse(fs.readFileSync(db));
 
 /**
  * Server setup
@@ -35,9 +36,43 @@ const middlewares = jsonServer.defaults();
 
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
+// Add specific routing handles that are not managed
+server.use((req, res, next) => {
+    switch (req.method) {
+        case "GET":
+            console.log(req.query)
+            // console.log(router)
+            // res.jsonp(dbInMem.jobs2.filter(record => record.owner = req.query.owner))
+            console.log(req.query.owner)
+            const regexp = req.query.owner === "*" ? `\w*` : req.query.owner;
+
+            const response = _.filter(dbInMem.jobs2, (record) => record.owner.match(regexp) & record.jobid.match(req.query.jobid));
+            const owners = _.filter(dbInMem.jobs2, (record) => record.owner.match(regexp));
+            const jobs = _.filter(owners, (record) => record.jobid.match(req.query.jobid));
+            res.jsonp(jobs)
+            break;
+    
+        default:
+            next();
+    }
+})
+// server.use((req, res, next) => {
+    //     if (req.method === 'GET') {
+        //         req.url = "/jobs"
+        //     //   req.url = decodeURIComponent(req.url);
+        //     //   req.originalUrl = decodeURIComponent(req.originalUrl);
+        //     //   req._parsedUrl.search = decodeURIComponent(req._parsedUrl.search);
+        //     //   req._parsedUrl.query = decodeURIComponent(req._parsedUrl.query);
+        //     //   req._parsedUrl.path = decodeURIComponent(req._parsedUrl.path);
+        //     //   req._parsedUrl.href = decodeURIComponent(req._parsedUrl.href);
+        //     //   req._parsedUrl._raw = decodeURIComponent(req._parsedUrl._raw);
+        //       console.log(req.url)
+        //     }
+        //     // Continue to JSON Server router
+        //     next()
+    //   })
 // Order matters! rewriter needs to be before router 
 server.use(rewriter);
-// mount the router on a different endpoint
 server.use(router);
 
 /**
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 exports.normalizePort = (value) => {
     var port = parseInt(value, 10);
 
@@ -13,3 +15,15 @@ exports.normalizePort = (value) => {
 
     return false;
 };
+
+exports.generateRoutes = (seedFile) => {
+    const routes = {};
+    rawRoutes = JSON.parse(fs.readFileSync(seedFile)).routes;
+    if (rawRoutes.base) {
+        Object.assign(routes, rawRoutes.base);
+        Object.assign(routes, rawRoutes.custom);
+    } else {
+        Object.assign(routes, rawRoutes.custom);
+    }
+    return routes;
+}
